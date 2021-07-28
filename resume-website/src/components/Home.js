d1fa3c485327ca9617fe63efc1c6b929f989038f
Gregory Glatzer <gregoryg323@gmail.com>
2021-07-28 18:12:42 -0400

    updated resume file

--- resume-website/src/components/Home.js
+++ resume-website/src/components/Home.js
@@ -24,14 +24,14 @@ function Home() {
 
               <section className={classes.intro}>
                 <Typography variant="h6" className={classes.introParagraph}>
-                Hello! My name is Gregory Glatzer. I am a sophomore at Penn State University working towards a BS in Applied Data Science. 
+                Hello! My name is Gregory Glatzer. I am a Junior at Penn State University working towards a BS in Applied Data Science. 
                 I believe that I am always growing as a learner, and that the best way to increase my skills is to listen and practice as much as I can. 
                 Whether it is side projects such as building AI for board games, or getting involved with a startup that implements AI into the medical field, 
                 I try to immerse myself into the world of all things data science and seize every opportunity I can get!
                 </Typography>
                 <Typography variant="h6" className={classes.introParagraph}>
                 In addition to my passion for all things coding and data, I also enjoy playing music! I play classical clarinet at a high level, 
-                which has gotten me to the national level of concert bands and an opportunity to play at Carnegie Hall! I also enjoy listening to and playing jazz.
+                which has gotten me to the national level of concert bands and an opportunity to play at Carnegie Hall! I also enjoy listening to and playing jazz. I am minoring in Jazz Performance.
                 </Typography>
                 <Typography variant="h6" className={classes.introParagraph}>
                 Please enjoy browsing through my resume, and feel free to reach out with any questions!
