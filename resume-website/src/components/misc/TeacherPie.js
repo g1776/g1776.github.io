f402d54d4148cb6bc4f01fbf24ef014e92c790ba
Gregory Glatzer <gregoryg323@gmail.com>
2021-08-16 12:28:18 -0400

    Basic Teacher Pie Works

--- resume-website/src/components/misc/TeacherPie.js
+++ resume-website/src/components/misc/TeacherPie.js
@@ -13,15 +13,64 @@ import {
 
 const Header = (props) => <Box style={{textAlign: "center"}}><Typography variant="h3" style={{fontWeight: 1000, marginBottom: '100px'}}>{props.children}</Typography></Box>
 
+const colors = {
+    aqua: "#00ffff",
+    azure: "#f0ffff",
+    beige: "#f5f5dc",
+    black: "#000000",
+    blue: "#0000ff",
+    brown: "#a52a2a",
+    cyan: "#00ffff",
+    darkblue: "#00008b",
+    darkcyan: "#008b8b",
+    darkgrey: "#a9a9a9",
+    darkgreen: "#006400",
+    darkkhaki: "#bdb76b",
+    darkmagenta: "#8b008b",
+    darkolivegreen: "#556b2f",
+    darkorange: "#ff8c00",
+    darkorchid: "#9932cc",
+    darkred: "#8b0000",
+    darksalmon: "#e9967a",
+    darkviolet: "#9400d3",
+    fuchsia: "#ff00ff",
+    gold: "#ffd700",
+    green: "#008000",
+    indigo: "#4b0082",
+    khaki: "#f0e68c",
+    lightblue: "#add8e6",
+    lightcyan: "#e0ffff",
+    lightgreen: "#90ee90",
+    lightgrey: "#d3d3d3",
+    lightpink: "#ffb6c1",
+    lightyellow: "#ffffe0",
+    lime: "#00ff00",
+    magenta: "#ff00ff",
+    maroon: "#800000",
+    navy: "#000080",
+    olive: "#808000",
+    orange: "#ffa500",
+    pink: "#ffc0cb",
+    purple: "#800080",
+    violet: "#800080",
+    red: "#ff0000",
+    silver: "#c0c0c0",
+};
+
+const randomColor = () => {
+    var result;
+    var count = 0;
+    for (var prop in colors)
+        if (Math.random() < 1/++count)
+        result = prop;
+    return { name: result, rgb: colors[result]};
+    };
 
 const TeacherPie = (props) => {
 
-    const [pie, setPie] = useState([])
-
-    useEffect(() => {
-        console.log("PIE:")
-        console.log(pie)
-    }, [pie])
+    const pie = useRef([])
+    const answers = useRef([])
+    const [done, setDone] = useState(false)
 
 
     const Question = (props) => {
@@ -31,18 +80,31 @@ const TeacherPie = (props) => {
             setAnswer(newAnswer)
         }
 
-        const handleClick = useCallback(() => {
-            console.log(answer)
+        const handleClick = () => {
+            // update list of answers
+            answers.current = [{question: props.children, answer: answer},...answers.current]
+
+            if (answer.slices !== undefined) {
+                const existingColors = pie.current.map(slice => slice.color)
+                const newPie = answer.slices.map(slice => {
+                    // get unique color for each slice
+                    let color = randomColor();
+                    while (existingColors.findIndex(c => c === color) !== -1) {
+                        color = randomColor()
+                    }
+                    return {value: slice.value, label: slice.label, color: color}
+                })
+
+                pie.current = pie.current.concat(newPie);
+            }
             if (answer.question === undefined){
                 console.log("Now time to display your pie...");
-            if (answer.slices !== undefined) {
-                const newPie = answer.slices.map(slice => ({value: slice.value, label: slice.label}))
-                setPie([...pie, ...newPie]);
-            }
+                console.log(pie.current)
+                setDone(true)
             } else {
                 props.onClick(answer.question);
             }
-        }, [answer])
+        }
         
 
         return (
@@ -74,154 +136,233 @@ const TeacherPie = (props) => {
     // Questions
 
     const QDataAvailableTeacherIEP = <Question
-        onClick={q => setCurrentQ(q)}
-        options={[
-            <MenuItem value={{
-                value: "Yes",
-                slices: [
-                    {value: 10, label: "TSD (2 or 3 Measures)"},
-                ]
-            }}>
-                Yes
-            </MenuItem>,
-            <MenuItem value={{
-                value: "No",
-                slices: [
-                    {value: 10, label: "TSD (1 Measure)"},
-                ]
-            }}>
-                No
-            </MenuItem>
-        ]}
-    >
-    Do you have IEP Goals?
-    </Question>
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{
+                    value: "Yes",
+                    slices: [
+                        {value: 10, label: "TSD (2 or 3 Measures)"},
+                    ]
+                }}>
+                    Yes
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "No",
+                    slices: [
+                        {value: 10, label: "TSD (1 Measure)"},
+                    ]
+                }}>
+                    No
+                </MenuItem>
+            ]}
+        >
+        Do you have IEP Goals?
+        </Question>
+
 
     const QTeacherOtherIEP = <Question
-        onClick={q => setCurrentQ(q)}
-        options={[
-            <MenuItem value={{
-                value: "Yes",
-                slices: [
-                    {value: 10, label: "TSD"},
-                    {value: 10, label: "LEA"},
-                ]
-            }}>
-                Yes
-            </MenuItem>,
-            <MenuItem value={{
-                value: "No",
-                slices: [
-                    {value: 20, label: "LEA"},
-                ]
-            }}>
-                No
-            </MenuItem>
-        ]}
-    >
-    Do you have IEP Goals?
-    </Question>
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{
+                    value: "Yes",
+                    slices: [
+                        {value: 10, label: "TSD"},
+                        {value: 10, label: "LEA"},
+                    ]
+                }}>
+                    Yes
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "No",
+                    slices: [
+                        {value: 20, label: "LEA"},
+                    ]
+                }}>
+                    No
+                </MenuItem>
+            ]}
+        >
+        Do you have IEP Goals?
+        </Question>
+
 
     const QClassroomTeacherData = <Question
-        onClick={q => setCurrentQ(q)}
-        options={[
-            <MenuItem value={{
-                value: "Data-Available Teacher",
-                question: QDataAvailableTeacherIEP,
-                slices: [
-                    {value: 10, label: "LEA"},
-                    {value: 70, label: "OP"},
-                    {value: 10, label: "BL"}
-                ]
-            }}>
-                Data-Available Teacher
-            </MenuItem>,
-            <MenuItem value={{
-                value: "Non-Data-Available Teacher",
-                question: QTeacherOtherIEP,
-                slices: [
-                    {value: 70, label: "OP"},
-                    {value: 10, label: "BL"}
-                ]
-            }}>
-                Non-Data-Available Teacher
-            </MenuItem>,
-            <MenuItem value={{
-                value: "Teacher Without Building-level Data",
-                question: QTeacherOtherIEP,
-                slices: [
-                    {value: 80, label: "OP"}
-                ]
-            }}>
-                Teacher Without Building-level Data
-            </MenuItem>
-        ]}
-    >
-    What level of data do you have?
-    </Question>
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{
+                    value: "Data-Available Teacher",
+                    question: QDataAvailableTeacherIEP,
+                    slices: [
+                        {value: 10, label: "LEA"},
+                        {value: 70, label: "OP"},
+                        {value: 10, label: "BL"}
+                    ]
+                }}>
+                    Data-Available Teacher
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "Non-Data-Available Teacher",
+                    question: QTeacherOtherIEP,
+                    slices: [
+                        {value: 70, label: "OP"},
+                        {value: 10, label: "BL"}
+                    ]
+                }}>
+                    Non-Data-Available Teacher
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "Teacher Without Building-level Data",
+                    question: QTeacherOtherIEP,
+                    slices: [
+                        {value: 80, label: "OP"}
+                    ]
+                }}>
+                    Teacher Without Building-level Data
+                </MenuItem>
+            ]}
+        >
+        What level of data do you have?
+        </Question>
 
 
     const QPrincipalData = <Question
-        onClick={q => setCurrentQ(q)}
-        options={[
-            <MenuItem value={{
-                value: "Principal With Building-level data",
-                slices: [
-                    {value: 70, label: "OP"},
-                    {value: 20, label: "PG"},
-                    {value: 10, label: "BL"}
-                ]
-            }}>
-                Principal With Building-level data
-            </MenuItem>,
-            <MenuItem value={{
-                value: "Principal Without Building-level data",
-                slices: [
-                    {value: 80, label: "OP"},
-                    {value: 20, label: "PG"}
-                ]
-            }}>
-                Principal Without Building-level data
-            </MenuItem>,
-        ]}
-    >
-        What level of data do you have?
-    </Question>
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{
+                    value: "Principal With Building-level data",
+                    slices: [
+                        {value: 70, label: "OP"},
+                        {value: 20, label: "PG"},
+                        {value: 10, label: "BL"}
+                    ]
+                }}>
+                    Principal With Building-level data
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "Principal Without Building-level data",
+                    slices: [
+                        {value: 80, label: "OP"},
+                        {value: 20, label: "PG"}
+                    ]
+                }}>
+                    Principal Without Building-level data
+                </MenuItem>,
+            ]}
+        >
+            What level of data do you have?
+        </Question>
 
 
+    const QNonTeachingData = <Question
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{
+                    value: "Non-teaching Professional With Building-level data",
+                    slices: [
+                        {value: 90, label: "OP"},
+                        {value: 10, label: "BL"}
+                    ]
+                }}>
+                    Non-teaching Professional With Building-level data
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "Non-teaching Professional Without Building-level data",
+                    slices: [
+                        {value: 100, label: "OP"},
+                    ]
+                }}>
+                    Non-teaching Professional Without Building-level data
+                </MenuItem>
+            ]}
+            >
+            What level of data do you have?
+            </Question>
 
-    const [currentQ, setCurrentQ] = useState(<Question
-        onClick={q => setCurrentQ(q)}
-        options={[
-            <MenuItem value={{value: "Non-teaching Professional", question: undefined}}>Non-teaching Professional</MenuItem>,
-            <MenuItem value={{value: "Classroom Teacher", question: QClassroomTeacherData}}>Classroom Teacher</MenuItem>,
-            <MenuItem value={{value: "Principal", question: QPrincipalData}}>Principal</MenuItem>
-        ]}
-        >What is your role?
-    </Question>)
 
+    const QRole = <Question
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{value: "Non-teaching Professional", question: QNonTeachingData}}>Non-teaching Professional</MenuItem>,
+                <MenuItem value={{value: "Classroom Teacher", question: QClassroomTeacherData}}>Classroom Teacher</MenuItem>,
+                <MenuItem value={{value: "Principal", question: QPrincipalData}}>Principal</MenuItem>
+            ]}
+            >What is your role?
+        </Question>
+
+
+    const QTemp = <Question
+            onClick={q => setCurrentQ(q)}
+            options={[
+                <MenuItem value={{
+                    value: "Yes",
+                    slices: [
+                        {value: 100, label: "OP"},
+                    ]
+                }}>
+                    Yes
+                </MenuItem>,
+                <MenuItem value={{
+                    value: "No",
+                    question: QRole
+                }}>
+                    No
+                </MenuItem>
+            ]}
+            >
+            Are you a temporary employee?
+        </Question>
+
+
+    const [currentQ, setCurrentQ] = useState(QTemp)
 
     return (
-        <Container style={{backgroundColor:"white", height: "100%", paddingTop: '30px', textAlign: "center"}}>
-            <Header>Build your pie chart</Header>
-            {currentQ}
-        </Container>
-
-
+        <>
+            {!done ?
+            <>
+                <div style={{backgroundColor:"white", height: "100%", paddingTop: '30px', textAlign: "center"}}>
+                <Header>Build your pie chart</Header>
+                {currentQ}
+                </div>
+            </>
+            :
+            
+                <Grid container>
+                    <Grid item xs={5} style={{backgroundColor: "whitesmoke", float: "left", borderRight: "2px solid #333"}}>
+                        <Container style={{paddingTop: '20px'}}>
+                            <Box style={{textAlign: "center"}}><Typography variant="h3" style={{fontWeight: 1000, marginBottom: '30px'}}>Your Answers</Typography></Box>
+                            {
+                                answers.current.map(answer => {
+                                return (
+                                    <>
+                                    <Typography variant="h5">{answer.question}</Typography>
+                                    <Typography variant="body1" style={{marginBottom: '15px'}}>{answer.answer.value}</Typography>
+                                    </>
+                                )
+                            })
+                            }
+                        </Container>
+                    </Grid>
+                    <Grid item xs={7}>
+                    <div style={{backgroundColor:"white", height: "100vh", textAlign: "center"}}>
+                        <PieChart
+                            data={
+                                pie.current.map(slice => ({ title: slice.label, value: slice.value, color: slice.color.rgb}))
+                                }
+                            animate
+                            animationDuration={1000}
+                            label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} % ${dataEntry.title}`}
+                            labelStyle={{fontSize: '1px', fontFamily: 'sans-serif'}}
+                            radius={12}
+                            labelPosition={112}
+                            center={[15,15]}
+                            viewBoxSize={[30,30]}    
+                        />
+                        </div>
+                    </Grid>
+                </Grid>
+            }
+        </>
     )
 }
 
 export default TeacherPie
-
-
-
-{/* <Grid item xs={6}>
-<PieChart
-     data={[
-         { title: 'One', value: 10, color: '#E38627' },
-         { title: 'Two', value: 15, color: '#C13C37' },
-         { title: 'Three', value: 20, color: '#6A2135' },
-     ]}
-     style={{padding: '100px'}}
- />;
- </Grid>  */}
